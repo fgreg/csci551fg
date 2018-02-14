70d6e029305dec9bb4450bdf0ac14021cc23efe3
fgreg <fgreguska+github@gmail.com>
2018-02-13 21:28:19 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
    name='csci551fg',
-   version='1.0',
+   version='proja.stage2',
    packages=find_packages(),
-   url='',
-   license='',
-   author='Frank',
+   url='https://github.com/fgreg/csci551fg',
+   license=open('LICENSE.txt').text,
+   author='Frank Greguska',
    author_email='greguska@usc.edu',
-   description='',
+   description='Module for USC CSCI 551 Spring 2018 that completes the practical project requirements.',
    include_package_data=True
 )
